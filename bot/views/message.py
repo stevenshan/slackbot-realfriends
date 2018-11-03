9c041dd17b4dca3154148208edd7befd10d5c08b
Steven Shan <steven.shan99@gmail.com>
2018-11-03 23:17:44 +0000

    added response trigger

--- bot/views/message.py
+++ bot/views/message.py
@@ -4,21 +4,35 @@ when a message is received or changed
 
 from bot.views import common, file
 import requests
+from base64 import b64encode, b64decode
+
+def decode(base64):
+    return b64decode(base64.encode("ascii")).decode("ascii")
+
+trigger = decode("RnVja1N0ZXZlbigpOw==")
+responseWord = decode("ZnVjaw==")
 
 def message(data):
     subtype = common.get(data, "event", "subtype")
+    channel = common.get(data, "event", "channel")
+    user = common.get(data, "event", "user")
 
     if subtype == "file_share":
         result = file.created(data["event"]["files"][0], as_file=True)
 
         if result is not None:
-            user = str(common.get(data, "event", "user"))
 
             if str(result) == "":
                 text = "<@%s>: _Empty Response_" % user
             else:
                 text = "<@%s>: ```%s```" % (user, result)
 
-            common.postMessage(common.get(data, "event", "channel"), text)
+            common.postMessage(channel, text)
+    elif common.get(data, "event", "text").strip(" ") == trigger:
+        entity = user
+        if entity is None or entity == "":
+            entity = str(common.get(data, "event", "username"))
+        common.postMessage(channel,
+            "No %s you <@%s>" % (responseWord, entity))
 
     return ""
